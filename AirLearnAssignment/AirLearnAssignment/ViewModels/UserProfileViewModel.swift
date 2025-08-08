358b1f8552c992e19042a389a7102aecc3488e1f
Aakash Narkar <aakash.narkar@ril.com>
2025-08-08 19:23:29 +0530

    loaded apis in asyn manner

--- AirLearnAssignment/AirLearnAssignment/ViewModels/UserProfileViewModel.swift
+++ AirLearnAssignment/AirLearnAssignment/ViewModels/UserProfileViewModel.swift
@@ -21,9 +21,7 @@ class UserProfileViewModel: ObservableObject {
     init(user: GitHubUser, githubService: GitHubServiceProtocol = GitHubService()) {
         self.user = user
         self.githubService = githubService
-        fetchUser()
-        fetchUserFollowers()
-        fetchRepositories()
+        self.loadDataAsync()
     }
     
     func fetchUser() {
@@ -63,6 +61,29 @@ class UserProfileViewModel: ObservableObject {
                     self?.error = error
                 }
             } receiveValue: { [weak self] repositories in
+                self?.repositories = repositories
+            }
+            .store(in: &cancellables)
+    }
+    
+    func loadDataAsync() {
+        isLoading = true
+        error = nil
+
+        let userPublisher = githubService.fetchUser(username: user.login)
+        let followersPublisher = githubService.fetchFollowers(username: user.login)
+        let reposPublisher = githubService.fetchUserRepositories(username: user.login)
+
+        Publishers.Zip3(userPublisher, followersPublisher, reposPublisher)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                self?.isLoading = false
+                if case .failure(let err) = completion {
+                    self?.error = err
+                }
+            } receiveValue: { [weak self] user, followers, repositories in
+                self?.user = user
+                self?.followers = followers.count
                 self?.repositories = repositories
             }
             .store(in: &cancellables)
